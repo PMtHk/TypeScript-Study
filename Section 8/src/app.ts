3947fc5ed98e86c6676f6848cfd462a44a59c7f3
PMtHk <zooby88@naver.com>
2022-08-22 17:32:58 +0900

    about Decorators

---
+++ Section 8/src/app.ts
@@ -0,0 +1,107 @@
+// function Logger(constructor: Function) {
+//   console.log('Logging...');
+//   console.log(constructor);
+// }
+
+function Logger(logString: string) {
+  console.log('logger factory');
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
+}
+
+function WithTemplate(template: string, hookId: string) {
+  console.log('template factory');
+  return function <T extends { new (...args: any[]): { name: string } }>(
+    originalConstructor: T
+  ) {
+    return class extends originalConstructor {
+      constructor(..._: any[]) {
+        super();
+        console.log('Rendering template');
+        const hookEl = document.getElementById(hookId);
+        // const p = new originalConstructor();
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector('h1')!.textContent = this.name;
+        }
+      }
+    };
+  };
+}
+
+// 아래가 먼저 실행된다. WithTemplate -> Logger 순
+@Logger('Logging - Person') // decorator
+@WithTemplate('<h1>My Person Object</h1>', 'app')
+class Person {
+  name = 'zooby';
+
+  constructor() {
+    console.log('Creating person object...');
+  }
+}
+
+const pers = new Person();
+
+console.log(pers);
+
+//-----------
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log('Property Decorator!');
+  console.log(target, propertyName);
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log('Accessor Decorator!');
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log3(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log('Method Decorator!');
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log('Parameter Decorator!');
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error('Invalid price - should be positive!');
+    }
+  }
+
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
+    return this._price * (1 + tax);
+  }
+}
+
+const p1 = new Product('Book', 19);
+const p2 = new Product('Book 2', 29);
