a53fafc244e51b0156e7e0180b399260e1faa49d
PMtHk <zooby88@naver.com>
2022-08-29 17:35:01 +0900

    Section 14 : React.js with Ts

--- Section 14/section-14/src/App.tsx
+++ Section 14/section-14/src/App.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
+import { Todo } from './todo.model';
+
+const App: React.FC = () => {
+  // FC stands for FunctionComponent
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const todoAddHandler = (text: string) => {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
+  };
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.id !==todoId)
+    })
+  }
+
   return (
-    <div className="App">
-      
+    <div className='App'>
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} onDeleteTodo = {todoDeleteHandler}/>
     </div>
   );
-}
+};
 
 export default App;
